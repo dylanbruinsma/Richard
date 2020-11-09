9d8f41683350102ceb68035055e0355561932751
dylono96 <dylanbruinsma@hotmail.nl>
2020-11-09 20:03:26 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,7 +6,7 @@ client = commands.Bot(command_prefix='pp ')
 token = os.getenv("DISCORD_BOT_TOKEN")
 # token = 'NzczMTg5MjE0ODExNTg2NTYy.X6FmkQ.zLMqc-de9QTKoYWsAAYAhlaFFeQ'
 
-for filename in os.listdir('./cogs'):
+for filename in os.listdir('cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
