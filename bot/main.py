d8ce159b1e3718717fb498a8dba86fd2a927ea6f
dylono96 <dylanbruinsma@hotmail.nl>
2020-11-12 11:46:00 +0100

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -5,14 +5,12 @@ import os
 client = commands.Bot(commands.when_mentioned_or('pp '))
 token = os.getenv("DISCORD_BOT_TOKEN")
 
+
 @client.event
 async def on_ready():
     await client.change_presence(
         activity=discord.Activity(type=discord.ActivityType.listening, name='you'))
     print('Richard is er klaar voor!')
 
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(token)
