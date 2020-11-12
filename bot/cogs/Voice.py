b97fecb079658ed3b5a200a3e5047b8027e71a58
dylono96 <dylanbruinsma@hotmail.nl>
2020-11-12 14:02:05 +0100

    Update Voice.py

--- bot/cogs/Voice.py
+++ bot/cogs/Voice.py
@@ -4,7 +4,6 @@ import requests
 import os
 import soundcloud
 import random
-import time
 import asyncio
 
 
@@ -18,22 +17,27 @@ class Voice(commands.Cog):
         channel = self.client.get_channel(773155351485874216)
         vc = await channel.connect()
         # create a client object with your app credentials
-        client = soundcloud.Client(client_id='679195bf4a6d645ba1e74291c02d7a72')
-                
-        tracklist = []
-        list = client.get('/resolve', url='https://soundcloud.com/perfectmsc/sets/lofi')
-        ctx.channel.send('working')
-        playlist = client.get('/playlists/' + str(list.id))
+        vclient = soundcloud.Client(client_id='679195bf4a6d645ba1e74291c02d7a72')
 
-        for track in playlist.tracks:
-            tracklist.append((track['id'], track['title']))
+        sets = ['https://soundcloud.com/jennifer-naremskaya/sets/gg-magree-1',
+                'https://soundcloud.com/perfectmsc/sets/lofi',
+                'https://soundcloud.com/sanholobeats/sets/album1',
+                'https://soundcloud.com/mugatunesofficial/sets/audible-adderall-14'
+                ]
+        tracklist = []
+        for setx in sets:
+
+            list = vclient.get('/resolve', url=setx)
+            playlist = vclient.get('/playlists/' + str(list.id))
+
+            for track in playlist.tracks:
+                tracklist.append((track['id'], track['title']))
         random.shuffle(tracklist)
-        print('added: %s songs to the list' % (len(tracklist)))
+        print(f'added: {len(tracklist)} songs to the list')
         for x in tracklist:
-            ctx.channel.send('working2')
             if not vc.is_playing():
                 print(x[1])
-                song = client.get('/tracks/' + str(x[0]) + '/stream', allow_redirects=False).location
+                song = vclient.get('/tracks/' + str(x[0]) + '/stream', allow_redirects=False).location
                 vc.play(discord.FFmpegPCMAudio(song))
                 await self.client.change_presence(
                     activity=discord.Activity(type=discord.ActivityType.listening, name=(str(x[1]))))
