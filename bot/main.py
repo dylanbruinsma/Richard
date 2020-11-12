9ca72af67172b42b5f5ee73eac2b5ae9f49575d9
dylono96 <dylanbruinsma@hotmail.nl>
2020-11-12 12:30:52 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,10 +5,6 @@ import os
 client = commands.Bot(commands.when_mentioned_or('pp '))
 token = os.getenv("DISCORD_BOT_TOKEN")
 
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
 
 @client.event
 async def on_ready():
