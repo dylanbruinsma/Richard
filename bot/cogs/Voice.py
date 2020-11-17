4366df3493dd77de063ba88e00fbeaaca702e5f1
dylono96 <dylanbruinsma@hotmail.nl>
2020-11-17 15:17:21 +0100

    Update Voice.py

--- bot/cogs/Voice.py
+++ bot/cogs/Voice.py
@@ -1,50 +1,74 @@
-import discord
-from discord.ext import commands
-import requests
-import os
-import soundcloud
-import random
 import asyncio
+import sqlite3
+import discord
+import youtube_dl
+from discord.ext import commands
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-class Voice(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.command()
-    async def play(self, ctx):
+    async def stream(self, ctx):
         await ctx.channel.purge(limit=1)
-        channel = self.client.get_channel(774957379061547038)
+        url = 'https://www.youtube.com/watch?v=5qap5aO4i9A'
+        channel = self.bot.get_channel(773155351485874216)
         vc = await channel.connect()
-        # create a client object with your app credentials
-        vclient = soundcloud.Client(client_id='679195bf4a6d645ba1e74291c02d7a72')
-        ctx.send('Hallo')
-        sets = ['https://soundcloud.com/jennifer-naremskaya/sets/gg-magree-1',
-                'https://soundcloud.com/perfectmsc/sets/lofi',
-                'https://soundcloud.com/sanholobeats/sets/album1',
-                'https://soundcloud.com/mugatunesofficial/sets/audible-adderall-14'
-                ]
-        tracklist = []
-        for setx in sets:
-            channel.send(setx)
-            list = vclient.get('/resolve', url=setx)
-            playlist = vclient.get('/playlists/' + str(list.id))
 
-            for track in playlist.tracks:
-                tracklist.append((track['id'], track['title']))
-        random.shuffle(tracklist)
-        ctx.send(f'added: {len(tracklist)} songs to the list')
-        for x in tracklist:
-            if not vc.is_playing():
-                print(x[1])
-                song = vclient.get('/tracks/' + str(x[0]) + '/stream', allow_redirects=False).location
-                vc.play(discord.FFmpegPCMAudio(song))
-                ctx.channel.send(f'ik speel nu: {song}')
-                await self.client.change_presence(
-                    activity=discord.Activity(type=discord.ActivityType.listening, name=(str(x[1]))))
-            while vc.is_playing():
-                await asyncio.sleep(0.1)
+        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+        vc.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await self.bot.change_presence(
+            activity=discord.Activity(type=discord.ActivityType.listening, name=player.title))
 
 
 def setup(client):
-    client.add_cog(Voice(client))
+    client.add_cog(Music(client))
